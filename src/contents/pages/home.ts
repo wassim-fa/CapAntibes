6bec1e309c9ad373b0ea32db3710fb7d59d032e0
Jérémy <fumeron.jeremy@live.fr>
2023-03-28 21:40:44 +0200

    feat(home): updates (#6)

--- src/contents/pages/home.ts
+++ src/contents/pages/home.ts
@@ -1,38 +1,88 @@
 import { IContentByLang } from '@/interfaces'
 
 export const contentsHome: Record<string, IContentByLang> = {
-  text1: {
-    fr: 'Redesigné par l’architecte Bernard Dubois, ce boutique-hôtel cinq étoiles accueille 35 chambres et suites, au bord de l’unique plage privée de la Côté d’Azur.',
-    en: 'en-Redesigné par l’architecte Bernard Dubois, ce boutique-hôtel cinq étoiles accueille 35 chambres et suites, au bord de l’unique plage privée de la Côté d’Azur.',
-    ru: 'ru-Redesigné par l’architecte Bernard Dubois, ce boutique-hôtel cinq étoiles accueille 35 chambres et suites, au bord de l’unique plage privée de la Côté d’Azur.'
+  title1: {
+    fr: `bienvenue au cap d’antibes`,
+    en: `en-bienvenue au cap d’antibes`,
+    ru: `ru-bienvenue au cap d’antibes`
+  },
+  text1_1: {
+    fr: `Le soleil. La mer. Le sable...`,
+    en: `en-Le soleil. La mer. Le sable...`,
+    ru: `ru-Le soleil. La mer. Le sable...`
+  },
+  text1_2: {
+    fr: `Bienvenue au Cap d’Antibes Beach Hôtel, 5 étoiles, les pieds dans l’eau. Posé sur la plage des Pêcheurs, c’est une bulle dédiée à la joie de vivre. Ce sont des bains de mer, seul au monde, au petit matin. Un déjeuner en bord de plage au restaurant BABA devenant une fête entre amis autour du bar. Un diner gastronomique aux Pécheurs. Des siestes, à l’abri dans des chambres spacieuses ou au calme d’un jardin exotique. On s’endort avec le bruit des vagues et une certitude. Demain il fera beau.`,
+    en: `en-Bienvenue au Cap d’Antibes Beach Hôtel, 5 étoiles, les pieds dans l’eau. Posé sur la plage des Pêcheurs, c’est une bulle dédiée à la joie de vivre. Ce sont des bains de mer, seul au monde, au petit matin. Un déjeuner en bord de plage au restaurant BABA devenant une fête entre amis autour du bar. Un diner gastronomique aux Pécheurs. Des siestes, à l’abri dans des chambres spacieuses ou au calme d’un jardin exotique. On s’endort avec le bruit des vagues et une certitude. Demain il fera beau.`,
+    ru: `ru-Bienvenue au Cap d’Antibes Beach Hôtel, 5 étoiles, les pieds dans l’eau. Posé sur la plage des Pêcheurs, c’est une bulle dédiée à la joie de vivre. Ce sont des bains de mer, seul au monde, au petit matin. Un déjeuner en bord de plage au restaurant BABA devenant une fête entre amis autour du bar. Un diner gastronomique aux Pécheurs. Des siestes, à l’abri dans des chambres spacieuses ou au calme d’un jardin exotique. On s’endort avec le bruit des vagues et une certitude. Demain il fera beau`
+  },
+  title2: {
+    fr: `les chambres et suites`,
+    en: `en-les chambres et suites`,
+    ru: `ru-les chambres et suites`
   },
   text2: {
-    fr: 'Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.',
-    en: 'Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.',
-    ru: 'ru-Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.Au cœur d’un jardin botanique, une piscine à débordementsurplombe la mer et un espace bien-être appelle au farniente.'
+    fr: `Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime. Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime.`,
+    en: `Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime. Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime.`,
+    ru: `ru-Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime. Avec 35 chambres et suites, le Beach est un véritable boutique hôtel de bord de mer. La plupart d’entre elles font face à la baie. Vous êtes dans votre chambre comme dans une maison rien qu’à vous, à la fois confortable et intime.`
+  },
+  title3_1: {
+    fr: `les pêcheurs,`,
+    en: `les pêcheurs,`,
+    ru: `les pêcheurs,`
+  },
+  title3_2: {
+    fr: `restaurant gastronomique`,
+    en: `restaurant gastronomique`,
+    ru: `restaurant gastronomique`
   },
   text3: {
-    fr: 'nos chambres deluxe, privilège, design, exécutive,COMMUNICANTES. nos chambres deluxe, privilège, design, exécutive, COMMUNICANTES.',
-    en: 'nos chambres deluxe, privilège, design, exécutive,COMMUNICANTES. nos chambres deluxe, privilège, design, exécutive, COMMUNICANTES.',
-    ru: 'ru-nos chambres deluxe, privilège, design, exécutive,COMMUNICANTES. nos chambres deluxe, privilège, design, exécutive, COMMUNICANTES.'
+    fr: `Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`,
+    en: `Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`,
+    ru: `ru-Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`
   },
-  text4: {
-    fr: 'au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé nos chambres deluxe,privilège, design, exécutive,COMMUNICANTES.',
-    en: 'au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé nos chambres deluxe,privilège, design, exécutive,COMMUNICANTES.',
-    ru: 'ru-au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé nos chambres deluxe,privilège, design, exécutive,COMMUNICANTES.'
+  title4_1: {
+    fr: `BABA`,
+    en: `BABA`,
+    ru: `BABA`
+  },
+  title4_2: {
+    fr: `Restaurant Bar Plage`,
+    en: `Restaurant Bar Plage`,
+    ru: `Restaurant Bar Plage`
+  },
+  text4_1: {
+    fr: `La musique et le soleil montent dans le ciel. Autour du bar en rotonde, l’ambiance est à l’insouciance. Baba, le restaurant à ciel ouvert, est orchestré par le chef Assaf Granit et propose une cuisine inspirée du Moyen-Orient. Il accueille les déjeuners qui se prolonge jusqu’au coucher du soleil.`,
+    en: `La musique et le soleil montent dans le ciel. Autour du bar en rotonde, l’ambiance est à l’insouciance. Baba, le restaurant à ciel ouvert, est orchestré par le chef Assaf Granit et propose une cuisine inspirée du Moyen-Orient. Il accueille les déjeuners qui se prolonge jusqu’au coucher du soleil.`,
+    ru: `ru-La musique et le soleil montent dans le ciel. Autour du bar en rotonde, l’ambiance est à l’insouciance. Baba, le restaurant à ciel ouvert, est orchestré par le chef Assaf Granit et propose une cuisine inspirée du Moyen-Orient. Il accueille les déjeuners qui se prolonge jusqu’au coucher du soleil.`
+  },
+  text4_2: {
+    fr: `Baba est un hymne à la fête et à la gaieté`,
+    en: `Baba est un hymne à la fête et à la gaieté`,
+    ru: `ru-Baba est un hymne à la fête et à la gaieté`
+  },
+  title5: {
+    fr: `FARNIENTE`,
+    en: `FARNIENTE`,
+    ru: `FARNIENTE`
   },
   text5: {
-    fr: 'au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé',
-    en: 'au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé',
-    ru: 'ru-au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé au restaurant gastronomique les pêcheurs, une cuisine locale par notre chef étoilé'
+    fr: `De petites criques. Quelques bateaux à l’abri d’une mer calme. Des chemins parmi une nature sauvage. La beauté naturelle du cap d’Antibes a souvent inspiré le cinéma. Entre les ports Gallice et du Crouton, le Cap d’Antibes Beach Hôtel est l’une des adresses historiques de la Côte d’Azur.`,
+    en: `De petites criques. Quelques bateaux à l’abri d’une mer calme. Des chemins parmi une nature sauvage. La beauté naturelle du cap d’Antibes a souvent inspiré le cinéma. Entre les ports Gallice et du Crouton, le Cap d’Antibes Beach Hôtel est l’une des adresses historiques de la Côte d’Azur.`,
+    ru: `ru-De petites criques. Quelques bateaux à l’abri d’une mer calme. Des chemins parmi une nature sauvage. La beauté naturelle du cap d’Antibes a souvent inspiré le cinéma. Entre les ports Gallice et du Crouton, le Cap d’Antibes Beach Hôtel est l’une des adresses historiques de la Côte d’Azur.`
+  },
+  title6: {
+    fr: `BIEN-ÊTRE`,
+    en: `BIEN-ÊTRE`,
+    ru: `BIEN-ÊTRE`
   },
   text6: {
-    fr: 'At aut quiate couid exceatem fuga Nam labor ape lanis que comnis iducipiet quis ea intii ssit, ipsam voluptur',
-    en: 'At aut quiate couid exceatem fuga Nam labor ape lanis que comnis iducipiet quis ea intii ssit, ipsam voluptur',
-    ru: 'ru-At aut quiate couid exceatem fuga Nam labor ape lanis que comnis iducipiet quis ea intii ssit, ipsam voluptur'
+    fr: `Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`,
+    en: `Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`,
+    ru: `ru-Le restaurant gastronomique Les Pêcheurs est une institution depuis les années 1940. Fidèle à la tradition, le chef Nicolas Rondelli travaille une carte mettant à l’honneur les produits de la Méditerranée. Dominant la mer, vous voilà comme sur le pont arrière d’un yacht du début du siècle.`
   },
   btnMoreInformation: {
-    fr: 'Plus d’informations',
+    fr: 'découvrir',
     en: 'More information',
     ru: 'ru-Plus d’informations'
   }
