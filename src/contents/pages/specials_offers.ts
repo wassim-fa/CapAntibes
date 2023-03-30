1a855783ffb08ab89003f5ccbd7583234d9486b1
Jérémy <fumeron.jeremy@live.fr>
2023-03-30 23:21:42 +0200

    feat(page): specials offers

--- src/contents/pages/specials_offers.ts
+++ src/contents/pages/specials_offers.ts
@@ -1,49 +1,49 @@
 import { IContentByLang } from '@/interfaces'
 
-export const contentsAddressRoom: Record<string, IContentByLang> = {
-    bouton: {
-        fr: 'Réserver',
-        en: 'Réserver',
-        ru: 'Réserver'
-    },
-    subTitle1: {
-        fr: `offre bien être`,
-        en: `offre bien être`,
-        ru: `offre bien être`,
-    },
-    chapter1: {
-        fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
-        en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
-        ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
-    },
-    subTitle2: {
-        fr: `OFFRE NUITEE ET GASTRONOMIE`,
-        en: `OFFRE NUITEE ET GASTRONOMIE`,
-        ru: `OFFRE NUITEE ET GASTRONOMIE`,
-    },
-    chapter2: {
-        fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-    },
-    subTitle3: {
-        fr: `RETRAITE YOGA & ÉCRITURE`,
-        en: `RETRAITE YOGA & ÉCRITURE`,
-        ru: `RETRAITE YOGA & ÉCRITURE`,
-    },
-    chapter3: {
-        fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-    },
-    subTitle4: {
-        fr: `OFFRE NUITEE ET PETIT DEJEUNER`,
-        en: `OFFRE NUITEE ET PETIT DEJEUNER`,
-        ru: `OFFRE NUITEE ET PETIT DEJEUNER`,
-    },
-    chapter4: {
-        fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
-        ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`
-    }
+export const contentsSpecialsOffersRoom: Record<string, IContentByLang> = {
+  toBook: {
+    fr: 'Réserver',
+    en: 'Réserver',
+    ru: 'Réserver'
+  },
+  subTitle1: {
+    fr: `offre bien être`,
+    en: `offre bien être`,
+    ru: `offre bien être`
+  },
+  text1: {
+    fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `
+  },
+  subTitle2: {
+    fr: `OFFRE NUITEE ET GASTRONOMIE`,
+    en: `OFFRE NUITEE ET GASTRONOMIE`,
+    ru: `OFFRE NUITEE ET GASTRONOMIE`
+  },
+  text2: {
+    fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`
+  },
+  subTitle3: {
+    fr: `RETRAITE YOGA & ÉCRITURE`,
+    en: `RETRAITE YOGA & ÉCRITURE`,
+    ru: `RETRAITE YOGA & ÉCRITURE`
+  },
+  text3: {
+    fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`
+  },
+  subTitle4: {
+    fr: `OFFRE NUITEE ET PETIT DEJEUNER`,
+    en: `OFFRE NUITEE ET PETIT DEJEUNER`,
+    ru: `OFFRE NUITEE ET PETIT DEJEUNER`
+  },
+  text4: {
+    fr: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    en: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`,
+    ru: `Offre na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante.`
+  }
 }
