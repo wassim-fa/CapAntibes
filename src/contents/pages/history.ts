3f2fc452dabe04f0bac1412d3f59acc6ecc11f15
Jérémy <fumeron.jeremy@live.fr>
2023-04-10 17:14:44 +0200

    feat(page): history added

---
+++ src/contents/pages/history.ts
@@ -0,0 +1,24 @@
+import { IContentByLang } from '@/interfaces'
+
+export const contentsHistory: Record<string, IContentByLang> = {
+  chapter1: {
+    fr: `l’histoire`,
+    en: `l’histoire`,
+    ru: `l’histoire`
+  },
+  text1: {
+    fr: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    en: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    ru: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `
+  },
+  chapter2: {
+    fr: `l’histoire`,
+    en: `l’histoire`,
+    ru: `l’histoire`
+  },
+  text2: {
+    fr: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    en: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `,
+    ru: `L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. L’histoire na nordiuropon Etris. Locciae invescia vero ve, loremp quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. `
+  }
+}
