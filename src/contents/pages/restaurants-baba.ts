145e306f3d00fc74de6e57f7c8f585fb193c6c51
Jérémy <fumeron.jeremy@live.fr>
2023-04-10 19:24:26 +0200

    feat(page): baba added

---
+++ src/contents/pages/restaurants-baba.ts
@@ -0,0 +1,99 @@
+import { IContentByLang } from '@/interfaces'
+
+export const contentsBabaRestaurant: Record<string, IContentByLang> = {
+  title: {
+    fr: `restaurant bar plage`,
+    en: `restaurant bar plage`,
+    ru: `restaurant bar plage`
+  },
+  intro: {
+    fr: `Baba restaurant etoilé signa nordiuropon etris. Decteri ssendem audem iusquod condem ad monuline turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum  et tunm effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Baba restaurant etoilé signa nordiuropon etris. Decteri ssendem audem iusquod condem ad monuline turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum  et tunm effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Baba restaurant etoilé signa nordiuropon etris. Decteri ssendem audem iusquod condem ad monuline turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum  et tunm effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  },
+  toBookTable: {
+    fr: `réserver une table`,
+    en: `réserver une table`,
+    ru: `réserver une table`
+  },
+  menu: {
+    fr: `la carte`,
+    en: `la carte`,
+    ru: `la carte`
+  },
+  chapter1: {
+    fr: `DÉJEUNER`,
+    en: `DÉJEUNER`,
+    ru: `DÉJEUNER`
+  },
+  subTitle1: {
+    fr: `de 7h30 à 10h30`,
+    en: `de 7h30 à 10h30`,
+    ru: `de 7h30 à 10h30`
+  },
+  text1: {
+    fr: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  },
+  chapter2: {
+    fr: `dîner`,
+    en: `dîner`,
+    ru: `dîner`
+  },
+  subTitle2: {
+    fr: `de 19h00 à 23h00`,
+    en: `de 19h00 à 23h00`,
+    ru: `de 19h00 à 23h00`
+  },
+  text2: {
+    fr: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  },
+  chapter3: {
+    fr: `Le bar`,
+    en: `Le bar`,
+    ru: `Le bar`
+  },
+  subTitle3: {
+    fr: `de 7h30 à 10h30`,
+    en: `de 7h30 à 10h30`,
+    ru: `de 7h30 à 10h30`
+  },
+  text3: {
+    fr: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  },
+  chapter4: {
+    fr: `Baba glace`,
+    en: `Baba glace`,
+    ru: `Baba glace`
+  },
+  subTitle4: {
+    fr: `de 7h30 à 10h30`,
+    en: `de 7h30 à 10h30`,
+    ru: `de 7h30 à 10h30`
+  },
+  text4: {
+    fr: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  },
+  chapter5: {
+    fr: `BABA BEACH POOL`,
+    en: `BABA BEACH POOL`,
+    ru: `BABA BEACH POOL`
+  },
+  subTitle5: {
+    fr: `de 7h30 à 10h30`,
+    en: `de 7h30 à 10h30`,
+    ru: `de 7h30 à 10h30`
+  },
+  text5: {
+    fr: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    en: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`,
+    ru: `Le matin signa nordiuropon etris. Decteri ssendem audem iusquod condem ad mo turebatiam vid consu inatodi ctorarit. Locciae invescia vero ve, quidendi pra, essenatabem consunum effreviverum ex se ante factatque miliniq uostrum int. An vermis, fursul vid sedite confer audende ntiaede mquit. Untrenihicae moc. Muliam ium urnum poenduc idiis. Rorsupi cavero in tabit.`
+  }
+}
