7e72a4de63364e72b2d022f7bd92c2a40105fdb9
Jérémy <fumeron.jeremy@live.fr>
2023-04-10 17:19:38 +0200

    feat(page): history added (#16)

---
+++ src/styles/pages/history.tsx
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const HistoryPageStyles = createGlobalStyle`
+  main#history {
+    margin-top: 80px;
+    .h3, .p {
+      line-height: 29px;
+    }
+    .h3 {
+      text-transform: uppercase;
+      font-size: 24px;
+      margin-bottom: 20px;
+    }
+    .p {
+      font-size: 17px;
+      p {
+        text-transform: none;
+      }
+    }
+    &.mobile {
+      .h3, .p {
+        line-height: 23px;
+      }
+      .h3 {
+        font-size: 20px;
+        margin-bottom: 15px;
+      }
+      .p {
+        font-size: 13px;
+        p {
+          text-transform: none;
+        }
+      }
+      .last-img {        
+        img {
+          height: auto;
+        }
+      }
+    }
+  }
+`
