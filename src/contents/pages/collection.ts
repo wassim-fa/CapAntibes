61f43ad53ed1bda06d03ff13d823f297f0bf318d
Jérémy <fumeron.jeremy@live.fr>
2023-04-10 08:46:03 +0200

    feat(page): collection page cleaned

--- src/contents/pages/collection.ts
+++ src/contents/pages/collection.ts
@@ -5,10 +5,10 @@ export const contentsCollection: Record<string, IContentByLang> = {
     fr: `Une succession de coups de cœur guidée par l’envie de créer des lieux uniques à l’image de leur destination. On dîne sous la belle étoile de la Butte Montmartre chez Monsieur Aristide, on s’installe dans une suite mystérieuse, cachée dans le jardin de Monsieur George. A Saint-Tropez, on s’encanaille au bord de l’eau sur un air de jazz à l’hôtel La Ponche. Ils font vivre aux voyageurs l’esprit, l’âme d’une ville ou d’un quartier. Ils sont une rencontre. Car nous sommes convaincus que l’hôtel dans lequel on se rend fait désormais partie du voyage. Qu’il doit raconter aussi une histoire. Les hôtels Adresses ne ressemblent à aucun autre. Ils sont éclectiques, festifs et élégants.`,
     en: `Une succession de coups de cœur guidée par l’envie de créer des lieux uniques à l’image de leur destination. On dîne sous la belle étoile de la Butte Montmartre chez Monsieur Aristide, on s’installe dans une suite mystérieuse, cachée dans le jardin de Monsieur George. A Saint-Tropez, on s’encanaille au bord de l’eau sur un air de jazz à l’hôtel La Ponche. Ils font vivre aux voyageurs l’esprit, l’âme d’une ville ou d’un quartier. Ils sont une rencontre. Car nous sommes convaincus que l’hôtel dans lequel on se rend fait désormais partie du voyage. Qu’il doit raconter aussi une histoire. Les hôtels Adresses ne ressemblent à aucun autre. Ils sont éclectiques, festifs et élégants.`,
     ru: `Une succession de coups de cœur guidée par l’envie de créer des lieux uniques à l’image de leur destination. On dîne sous la belle étoile de la Butte Montmartre chez Monsieur Aristide, on s’installe dans une suite mystérieuse, cachée dans le jardin de Monsieur George. A Saint-Tropez, on s’encanaille au bord de l’eau sur un air de jazz à l’hôtel La Ponche. Ils font vivre aux voyageurs l’esprit, l’âme d’une ville ou d’un quartier. Ils sont une rencontre. Car nous sommes convaincus que l’hôtel dans lequel on se rend fait désormais partie du voyage. Qu’il doit raconter aussi une histoire. Les hôtels Adresses ne ressemblent à aucun autre. Ils sont éclectiques, festifs et élégants.`
-  }, 
+  },
   button: {
     fr: `Découvrir`,
     en: `Découvrir`,
     ru: `Découvrir`
   }
-};
+}
