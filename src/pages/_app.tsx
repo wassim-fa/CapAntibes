d0fa968f83fc1e338dd1713c77c1532a866894a3
Jérémy <fumeron.jeremy@live.fr>
2023-05-15 09:16:30 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,26 @@ import { DefaultLayout } from '@/layouts'
 import { AppProps } from 'next/app'
 import React from 'react'
 
-function App({ Component, pageProps }: AppProps) {
+interface CustomPageProps extends AppProps {
+  isApple: boolean
+}
+function App({ Component, isApple, pageProps }: CustomPageProps) {
   return (
     <>
-      <DefaultLayout defaultLang={pageProps.language}>
+      <DefaultLayout defaultLang={pageProps.language} isApple={isApple}>
         <Component {...pageProps} />
       </DefaultLayout>
     </>
   )
+}
+
+App.getInitialProps = async (ctx: any) => {
+  const isApple = /Macintosh|MacIntel|MacPPC|Mac68K|iPhone|iPod/.test(
+    ctx.ctx.req.headers['user-agent']
+  )
+  console.log(ctx.ctx.req.headers['user-agent'])
+  console.log(`isApple: ${isApple}`)
+  return { isApple }
 }
 
 export default App
