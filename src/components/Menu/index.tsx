1d861f780f7ab295bab1c13e6641dcca3e8d3e30
Jérémy <fumeron.jeremy@live.fr>
2023-06-03 05:23:41 +0000

    manage size

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -97,8 +97,8 @@ const Menu = () => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="100"
-              height="20"
+              width="100%"
+              height="100%"
               viewBox="0 0 100 20"
             >
               <text x="0" y="15" fontSize={labelSize} fill="#d99b81">
