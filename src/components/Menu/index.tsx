1be6607f2b3fbe01a67b21c893104a8dc24e3580
Jérémy <fumeron.jeremy@live.fr>
2023-06-03 16:17:21 +0000

    test

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,6 +27,7 @@ const ItemText = ({ text }: { text: string }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       ref={svgRef}
+      width="100%"
       height="100%"
       viewBox="0 0 100 20"
     >
